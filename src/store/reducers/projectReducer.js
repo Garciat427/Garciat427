939eabf3f5fefaca318e4cef38eae14d4699cca8
sheenagarcia <tg1212@outlook.com>
2020-03-31 13:42:05 -0400

    Connected Redux store to dashboard and project lists

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+   projects:[
+      {id: '1', title: 'help me find the thingy', content: 'blah blah blah'},
+      {id: '2', title: 'Actually, help me find the other thingy', content: 'blah blah blah'},
+      {id: '3', title: 'NVMIND! help me find the last thingy', content: 'blah blah blah'},
+   ]
+}
 
 const projectReducer = (state = initState, action) => {
    return state

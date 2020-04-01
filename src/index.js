472e659b271b5aa487bdf2860eec378f4ed3917a
sheenagarcia <tg1212@outlook.com>
2020-04-01 14:01:34 -0400

    Added changes to index.js for a new updated version of firebase

--- src/index.js
+++ src/index.js
@@ -6,21 +6,34 @@ import * as serviceWorker from './serviceWorker';
 import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
-import { reduxFirestore, getFirestore } from 'redux-firestore'
-import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
+import { reduxFirestore, getFirestore, createFirestoreInstance} from 'redux-firestore'
+import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase'
 import fbConfig from './config/fbConfig'
+import firebase from 'firebase/app'
 
 import rootReducer from './store/reducers/rootReducer'
 
 const store = createStore(rootReducer,
-   compose(applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
-   reduxFirestore(fbConfig),
-   reactReduxFirebase(fbConfig)
-   
+   compose(
+      applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+      reduxFirestore(fbConfig)
    )
 );
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root')
+const rrfProps = {
+   firebase,
+   config: fbConfig,
+   dispatch: store.dispatch,
+   createFirestoreInstance
+};
+
+ReactDOM.render(
+   <Provider store={store}>
+      <ReactReduxFirebaseProvider {...rrfProps}>
+         <App />
+      </ReactReduxFirebaseProvider>
+   </Provider>, 
+   document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change

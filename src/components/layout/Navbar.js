74cbff4936656b005c0f1138bcf594655ab208f0
sheenagarcia <tg1212@outlook.com>
2020-04-01 23:47:50 -0400

    Added a fix where the navbar links would flash while the auth state loads

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -11,8 +11,8 @@ const Navbar = (props) => {
    const { auth } = props
    console.log(auth)
 
-   const links = auth.uid ? <SignedInLinks/> : <SignedOutLinks/>
-   return(
+   const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />
+   return (
       <nav className="nav-wrapper grey darken-3">
          <div className="container">
             <Link to='/' className="brand-logo">TestApp</Link>
--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import ReactDOM from 'react-dom';
 //Redux Dependencies
 import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './store/reducers/rootReducer'
-import { Provider } from 'react-redux'
+import { Provider, useSelector } from 'react-redux'
 import thunk from 'redux-thunk'
 
 //Firebase and Firestore Dependencies
 import fbConfig from './config/fbConfig'
 import firebase from 'firebase/app'
 import 'firebase/firestore'
-import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase'
+import { ReactReduxFirebaseProvider, getFirebase, isLoaded } from 'react-redux-firebase'
 import { reduxFirestore, getFirestore, createFirestoreInstance } from 'redux-firestore';
 
 import './index.css';
@@ -20,7 +20,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 //Firestore Initalization
-firebase.firestore() 
+firebase.firestore()
 
 
 const store = createStore(
@@ -35,17 +35,29 @@ const rrfProps = {
    firebase,
    config: fbConfig,
    dispatch: store.dispatch,
-   createFirestoreInstance
+   createFirestoreInstance,
+   userProfile: 'users', // where profiles are stored in database
+   presence: 'presence', // where list of online users is stored in database
+   sessions: 'sessions'
 };
+
+function AuthIsLoaded({ children }) {
+   const auth = useSelector(state => state.firebase.auth)
+   if (!isLoaded(auth)) return <div>Loading Screen...</div>;
+   return children
+}
 
 ReactDOM.render(
    <Provider store={store}>
       <ReactReduxFirebaseProvider {...rrfProps}>
-         <App />
+         <AuthIsLoaded>
+            <App />
+         </AuthIsLoaded>
       </ReactReduxFirebaseProvider>
    </Provider>,
    document.getElementById("root")
 );
+
 
 serviceWorker.unregister();
 

bb888ee29b91143388343b83ee29d7d95c596cfd
sheenagarcia <tg1212@outlook.com>
2020-04-01 23:31:38 -0400

    Added logic to navbar links to display the right set of links based on if the user is logged in or not

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -7,13 +7,16 @@ import SignedOutLinks from './SignedOutLinks'
 import { connect } from 'react-redux'
 
 
-const Navbar = () => {
+const Navbar = (props) => {
+   const { auth } = props
+   console.log(auth)
+
+   const links = auth.uid ? <SignedInLinks/> : <SignedOutLinks/>
    return(
       <nav className="nav-wrapper grey darken-3">
          <div className="container">
             <Link to='/' className="brand-logo">TestApp</Link>
-            <SignedInLinks/>
-            <SignedOutLinks/>
+            { links }
          </div>
       </nav>
 
@@ -21,9 +24,8 @@ const Navbar = () => {
 }
 
 const mapStateToProps = (state) => {
-   console.log(state);
    return {
-
+      auth: state.firebase.auth
    }
 }
 
